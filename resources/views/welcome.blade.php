a904862ee6102d2fe0287d8cd1b21e125b2f8968
burakhayirli <burakhayirli1@hotmail.com>
2021-01-20 15:08:32 +0200

    deneme

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -20,7 +20,7 @@
             }
         </style>
     </head>
-    Bu satır silinecek. Test
+    
     <body class="antialiased">
         <div class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center sm:pt-0">
             @if (Route::has('login'))
